2f54abd2373627681728ca55940a241d3ae3ab74
Patrick Shushereba <pshushereba@gmail.com>
2019-09-28 09:26:37 -0700

    Initial commit.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Import Redux State Management
+
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+// Import Reducer(s)
+
+import {smurfReducer} from './reducers/SmurfReducer.js'
+
+const store = createStore(
+    smurfReducer,
+    applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducers/SmurfReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    name: "",
+    age: "",
+    height: "",
+    id: null
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: return state;
+    }
+}
